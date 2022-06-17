920e38228db4c29695d1b82d54ca97e87d0307e5
xhfkd00 <xhfkd00@naver.com>
2022-06-17 12:28:47 +0900

    [feat] 강의상세 정보조회, 코스상세 정보조회 API구현

--- src/main/kotlin/com/codeUnicorn/codeUnicorn/domain/section/SectionDetailInfo.kt
+++ src/main/kotlin/com/codeUnicorn/codeUnicorn/domain/section/SectionDetailInfo.kt
@@ -7,25 +7,10 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
-// @Entity
-// @Table(name = "section")
-// data class Section(
-//     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     val id: Int? = null,
-//     @Column(name = "course_id")
-//     var courseId: Int,
-//     var name: String,
-//     @Column(name = "total_hours")
-//     var totalHours: Int,
-//     @Column(name = " lecture_count")
-//     var lectureCount: Int,
-// )
-
 @Entity
 @Table(name = "section")
 data class SectionDetailInfo(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     val sectionId: Int,
-
-    )
+)
