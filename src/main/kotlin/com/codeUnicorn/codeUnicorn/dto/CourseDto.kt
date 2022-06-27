40f4471fd0dc87000c525b7a4a61753a9918a479
xhfkd00 <xhfkd00@naver.com>
2022-06-27 18:07:21 +0900

    [feat] 관심 코스 등록 API 구현

---
+++ src/main/kotlin/com/codeUnicorn/codeUnicorn/dto/CourseDto.kt
@@ -0,0 +1,15 @@
+package com.codeUnicorn.codeUnicorn.dto
+
+import com.codeUnicorn.codeUnicorn.domain.likeCourse.LikeCourse
+
+data class CreateCourseLikeDto(
+    private val userId: Int?,
+    private val courseId: Int?,
+) {
+    fun toEntity(): LikeCourse {
+        return LikeCourse(
+            userId = this.userId,
+            courseId = this.courseId
+        )
+    }
+}
